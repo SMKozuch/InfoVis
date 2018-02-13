b85846710ce54b11511f0dc46738128a1d4128fa
Samuel Kožuch <kozuch.samuel@gmail.com>
2018-02-14 00:14:31 +0100

    Added data filtering in .js

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,9 +1,32 @@
-var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
+var margin = {
+    top: 100,
+    bottom: 100,
+    left: 50,
+    right: 50
+}
 
-var svg = d3.select("body").append("svg")
-          .attr("height","100%")
-          .attr("width","100%");
+var chosen_year = '2011'
 
-svg.selectAll("rect")
-    .data(dataArray)
-    .enter().append("rect");
+d3.csv('meteo.csv', function(data) {
+    return {
+        year: data.year,
+        month: data.month,
+        day: data.day,
+        temperature: data.temperature
+    };
+ }, 
+ function(d) {
+     var temp_by_year = d.filter(e => e.year === chosen_year)
+     .map(d => ({
+         month: d.month,
+         temperature: d.temperature
+     }));
+
+     var temp_avg = d3.nest()
+     .key(d => d.month)
+     .rollup(v => d3.mean(v, e => e.temperature / 10))
+     .entries(temp_by_year)
+
+     console.log(temp_avg)
+    }
+)
