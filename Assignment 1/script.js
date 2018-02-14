d5e61aeecf97e480723b0b6596474a9b7e437747
Samuel Kožuch <kozuch.samuel@gmail.com>
2018-02-14 02:23:36 +0100

    Minor changes
    Changes to syntax and variables

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,32 +1,25 @@
-var margin = {
-    top: 100,
-    bottom: 100,
-    left: 50,
-    right: 50
-}
-
-var chosen_year = '2011'
+var chosen_year = 2015
 
 d3.csv('meteo.csv', function(data) {
     return {
-        year: data.year,
+        year: +data.year,
         month: data.month,
         day: data.day,
         temperature: data.temperature
     };
  }, 
- function(d) {
-     var temp_by_year = d.filter(e => e.year === chosen_year)
-     .map(d => ({
-         month: d.month,
-         temperature: d.temperature
-     }));
+ function(a) {
+    var temp_by_year = a.filter(d => d.year === chosen_year)
+    .map(d => ({
+        month: d.month,
+        temperature: d.temperature
+    }));
 
-     var temp_avg = d3.nest()
-     .key(d => d.month)
-     .rollup(v => d3.mean(v, e => e.temperature / 10))
-     .entries(temp_by_year)
+    var avg_temp = d3.nest()
+    .key(d => d.month)
+    .rollup(v => d3.mean(v, d => d.temperature / 10))
+    .entries(temp_by_year)
 
-     console.log(temp_avg)
-    }
-)
+   console.log(avg_temp)
+ }        
+);
